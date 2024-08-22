54d28b73ebb2ccee5e02a1b610835dd8f2e71f14
LucasDRo <lucasdinizrodrigues123@gmail.com>
2024-08-22 20:01:51 -0300

    object item

---
+++ app/src/main/java/com/example/controledeestoque/model/item.java
@@ -0,0 +1,51 @@
+package com.example.controledeestoque.model;
+
+public class item {
+
+    private String nome;
+    private int qtde;
+    private float codigo;
+    private String categoria;
+
+    public item() {
+    }
+
+    public item (String nome, int qtde, float codigo, String categoria){
+        this.categoria=categoria;
+        this.codigo=codigo;
+        this.nome=nome;
+        this.qtde=qtde;
+    }
+
+    public void setNome(String nome){
+        this.nome=nome;
+    }
+
+    public String getNome(){
+        return nome;
+    }
+
+    public int getQtde() {
+        return qtde;
+    }
+
+    public void setQtde(int qtde) {
+        this.qtde = qtde;
+    }
+
+    public float getCodigo() {
+        return codigo;
+    }
+
+    public void setCodigo(float codigo) {
+        this.codigo = codigo;
+    }
+
+    public String getCategoria() {
+        return categoria;
+    }
+
+    public void setCategoria(String categoria) {
+        this.categoria = categoria;
+    }
+}
